3780e14fe6f6b580b116ac74923378354c65a629
Chaiyod Malingam <chaiyod.cymg@gmail.com>
2022-02-13 21:00:36 +0700

    change profile

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -28,7 +28,9 @@
 
                 <!-- Current Profile Photo -->
                 <div class="mt-2" x-show="! photoPreview">
+                    <a href="{{ $this->user->profile_photo_url }}">
                     <img src="{{ $this->user->profile_photo_url }}" alt="{{ $this->user->name }}" class="rounded-full h-20 w-20 object-cover">
+                     </a>               
                 </div>
 
                 <!-- New Profile Photo Preview -->
