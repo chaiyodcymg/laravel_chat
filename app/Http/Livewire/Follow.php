86ce72a5aa9dd4ab4ca4bb53f522a40243e13374
chaiyodcymg <chaiyod.cymg@gmail.com>
2022-03-11 22:03:42 +0700

    first commit

--- app/Http/Livewire/Follow.php
+++ app/Http/Livewire/Follow.php
@@ -54,7 +54,7 @@ class Follow extends Component
             $Follower->user_id = $this->user_id;
             $Follower->follower_id = Auth::user()->id;
             $Follower->save();
-            
+            dd();
         }else{
 
             $result_1 =   Following::onlyTrashed()->where('user_id', $result_following[0]->user_id)->where('following_id', $result_following[0]->following_id)->get();
